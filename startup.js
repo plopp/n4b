6f52c6dff999a400ba0d11dcf78497facaf075b0
Marcus Kempe <makem86@gmail.com>
2013-11-18 18:05:04 +0100

    Add support for PLC communication.

--- startup.js
+++ startup.js
@@ -2,4 +2,5 @@ Meteor.startup(function(){
 	console.log("Startup..");
   //console.log("Going through all timerules to see that everything is scheduled...");
   //Meteor.call('scheduleOccurrences');
+  
 });
