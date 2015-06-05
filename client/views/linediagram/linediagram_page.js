241599d4ccd13a2b144d5f87a042de4b50916fcd
Marcus Kempe <makem86@gmail.com>
2015-06-05 18:01:05 +0200

    Added line diagram.

---
+++ client/views/linediagram/linediagram_page.js
@@ -0,0 +1,33 @@
+Template.lineDiagramPage.helpers({
+	getValue : function(plcvar){
+		//return plcvar;
+      	var ret = Resources.findOne({plcVar: plcvar});
+      	if(ret !== undefined){
+      		return ret.value+ " °";
+      	}
+      	else{
+      		return "N/A";
+      	}
+  	},
+  	getFlow : function(plcvar){
+		//return plcvar;
+      	var ret = Resources.findOne({plcVar: plcvar});
+      	if(ret !== undefined){
+      		return ret.value+ " m³";
+      	}
+      	else{
+      		return "N/A";
+      	}
+  	}
+});
+
+Template.lineDiagramPage.onRendered(function() {
+	//$("#svgcontainer").svg({loadURL: '/img/Principscheman.svg',onLoad: drawIntro});
+	
+});
+
+function drawIntro(svg) { 
+	// svg.circle(130, 75, 50, {fill: 'none', stroke: 'red', strokeWidth: 3});
+	// var text1 = svg.text(250, 215, '13°', {fill: 'black', id:"vmm1"});
+	// $("#vmm1").text(Session.get("vmm1rettemp"));
+}
